67b1a3796656ec87c367af73731b96f96cab3b19
Nerya Manassen <50883311+ManassenN@users.noreply.github.com>
2022-05-25 18:06:13 +0300

    Inline Styling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+const date = new Date();
+const currentTime = date.getHours();
+
+let greeting;
+
+const customStyle = {
+  color: ""
+};
+
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good AfterNoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Evening";
+  customStyle.color = "blue";
+}
+
+console.log(currentTime);
+
 ReactDom.render(
   <div>
-    <h1> Good Morning </h1>
+    <h1 className="heading" style={customStyle}>
+      {greeting}
+    </h1>
   </div>,
   document.getElementById("root")
 );
